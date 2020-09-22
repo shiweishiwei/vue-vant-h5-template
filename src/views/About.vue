2bb3cd64156e209d3fa87c8b34fc13bc913d52bc
shiweishiwei <969530671@qq.com>
2020-09-22 09:32:45 +0800

    使用postcss-px-to-viewport实现移动端适配

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div class="test-viewport">测试转换</div>
 </template>
+
+<style scoped>
+.test-viewport {
+  width: 750px;
+  height: 100px;
+  font-size: 40px;
+  text-align: center;
+  line-height: 100px;
+  background: #13b5b1;
+}
+</style>
